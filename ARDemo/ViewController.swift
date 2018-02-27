355f63202182b5f0369c82eb82b28b244f0d4c96
Thomas Dubiel <td@crazynexus.de>
2018-02-27 22:43:50 +0100

    add more than one cube with light reflection and reset the world

--- ARDemo/ViewController.swift
+++ ARDemo/ViewController.swift
@@ -13,17 +13,17 @@ class ViewController: UIViewController {
    
    @IBOutlet weak var sceneView: ARSCNView!
    
+   let configuration: ARConfiguration = ARWorldTrackingConfiguration()
+   
    override func viewDidLoad() {
       super.viewDidLoad()
-
-      // pprepare the session by creating a World Tracking Configuration
-      let configuration = ARWorldTrackingConfiguration()
       
       // enable debug information
       sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
       
       // start the AR Sessuin
       sceneView.session.run(configuration)
+      sceneView.autoenablesDefaultLighting = true
    }
 
    override func didReceiveMemoryWarning() {
@@ -33,11 +33,36 @@ class ViewController: UIViewController {
    
    @IBAction func addCube(_ sender: Any) {
       let newNode = SCNNode()
-      newNode.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+      newNode.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+      newNode.geometry?.firstMaterial?.specular.contents = UIColor.lightGray
       newNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-      newNode.position = SCNVector3(0.0, 0.0, -0.3)
+      
+      let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+      let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+      let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+      
+      newNode.position = SCNVector3(x, y, z)
+      newNode.name = "Box"
       
       sceneView.scene.rootNode.addChildNode(newNode)
+   }
+   
+   @IBAction func reset(_ sender: Any) {
+      restartSession()
+   }
+   
+   func restartSession() {
+      sceneView.session.pause()
+      sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+         if node.name == "Box" {
+            node.removeFromParentNode()
+         }
+      }
+      sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+   }
+   
+   func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+      return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
    }
    
 }
